ee80a758b42b6c2f6123871600f9869c1472de37
DiegoDiaz9614 <diegodiaz624@yahoo.com>
2025-05-01 09:11:38 -0600

    CRUD operations

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const cors = require('cors');
+const mongodb = require('./db/connect')
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello from my route');
+app.use (cors());
+app.use(express.json());
+app.use((req,res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next()
 });
+app.use('/rolls', require('./routes/rolls'));
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+mongodb.initDb((err) => {
+    if(err) {
+        console.log('Database init failed', err);
+        return;
+    } else {
+        app.listen(PORT);
+        console.log(`Connected To Db And Listening on ${PORT}`)
+    }
+})
